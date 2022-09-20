34f4499ccaeb010c9bd44c6e18ae0b7c6ee71db1
Andreas Pappas <andrewpap1997@gmail.com>
2022-09-20 15:38:49 +0300

    add navbar menu items :white_check_mark:

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -8,23 +8,83 @@ import Link from 'next/link';
 
 import images from '../assets';
 
+/// Menu Items component
+const MenuItems = ({ isMobile, active, setActive }) => {
+  const generateLink = (index) => {
+    switch (index) {
+      case 0:
+        return '/';
+      case 1:
+        return '/created-nfts';
+      case 2:
+        return '/my-nfts';
+      default:
+        return '/';
+    }
+  };
+
+  return (
+    <ul
+      className={`list-none flexCenter flex-row ${
+        isMobile && 'flex-col h-full'
+      }`}
+    >
+      {['Explore NFTs', 'Listed NFTs', 'My NFTs'].map((item, index) => (
+        <li
+          key={index}
+          onClick={() => {
+            setActive(item);
+          }}
+          className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
+            active === item
+              ? 'dark:text-white text-nft-black-1'
+              : 'dark:text-nft-gray-3 text-nft-gray-2'
+          }`}
+        >
+          <Link href={generateLink(index)}>
+            {item}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
 const Navbar = () => {
   const { theme, setTheme } = useTheme();
+  const [active, setActive] = useState('Explore NFTs');
 
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
-          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
-            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="app-logo" />
-            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Unicorn 🦄</p>
+          <div
+            className="flexCenter md:hidden cursor-pointer"
+            onClick={() => {}}
+          >
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="app-logo"
+            />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">
+              Unicorn 🦄
+            </p>
           </div>
         </Link>
 
         {/* On medium screen devices remove the text and keep only the logo  */}
         <Link href="/">
           <div className="hidden md:flex" onClick={() => {}}>
-            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="app-logo" />
+            <Image
+              src={images.logo02}
+              objectFit="contain"
+              width={32}
+              height={32}
+              alt="app-logo"
+            />
           </div>
         </Link>
       </div>
@@ -42,11 +102,22 @@ const Navbar = () => {
             htmlFor="checkbox"
             className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
           >
-            <i className="fa-sun"><FontAwesomeIcon icon={faSun} /></i>
-            <i className="fa-moon"><FontAwesomeIcon icon={faMoon} /></i>
+            <i className="fa-sun">
+              <FontAwesomeIcon icon={faSun} />
+            </i>
+            <i className="fa-moon">
+              <FontAwesomeIcon icon={faMoon} />
+            </i>
             <div className="w-3 h-3 absolute bg-white rounded-full ball" />
           </label>
         </div>
+      </div>
+
+      {/* NavBar Menu Items for non mobile devices */}
+      <div className="md:hidden flex">
+        <ul className="list-none flexCenter flex-row">
+          <MenuItems active={active} setActive={setActive} />
+        </ul>
       </div>
     </nav>
   );
