5868502e40d8f9e81d41a4b052e79efc3b25d84c
Andreas Pappas <andrewpap1997@gmail.com>
2022-09-20 15:08:30 +0300

    add assets, add toggle dark-light mode, add responsive nav logo... :construction:

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,55 @@
-import React from 'react';
+import { useState, useEffect, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useTheme } from 'next-themes';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCoffee } from '@fortawesome/free-solid-svg-icons';
-import { faDiscord } from '@fortawesome/free-brands-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import Image from 'next/image';
+import Link from 'next/link';
 
-const Navbar = () => (
-  <div>
-    NavBar <FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faDiscord} />
-  </div>
-);
+import images from '../assets';
+
+const Navbar = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
+      <div className="flex flex-1 flex-row justify-start">
+        <Link href="/">
+          <div className="flexCenter md:hidden cursor-pointer" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="app-logo" />
+            <p className="dark:text-white text-nft-black-1 font-semibold text-lg ml-1">Unicorn 🦄</p>
+          </div>
+        </Link>
+
+        {/* On medium screen devices remove the text and keep only the logo  */}
+        <Link href="/">
+          <div className="hidden md:flex" onClick={() => {}}>
+            <Image src={images.logo02} objectFit="contain" width={32} height={32} alt="app-logo" />
+          </div>
+        </Link>
+      </div>
+
+      {/* Light / Dark mode toggle */}
+      <div className="flex flex-initial flex-row justify-end">
+        <div className="flex items-center mr-2">
+          <input
+            type="checkbox"
+            className="checkbox"
+            id="checkbox"
+            onChange={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+          />
+          <label
+            htmlFor="checkbox"
+            className="flexBetween w-8 h-4 bg-black rounded-2xl p-1 relative label"
+          >
+            <i className="fa-sun"><FontAwesomeIcon icon={faSun} /></i>
+            <i className="fa-moon"><FontAwesomeIcon icon={faMoon} /></i>
+            <div className="w-3 h-3 absolute bg-white rounded-full ball" />
+          </label>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Navbar;
