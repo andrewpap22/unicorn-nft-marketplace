13dcc8dfa84fe9da4125fc1e92eddcb96d37c879
Andreas Pappas <andrewpap1997@gmail.com>
2022-09-22 10:46:09 +0300

    fix nft info on query url params on NFTCard page :white_check_mark: and add upload file, drag and drop functionallity on create nft page :white_check_mark:

--- pages/create-nft.jsx
+++ pages/create-nft.jsx
@@ -1,7 +1,96 @@
-import React from 'react';
+import { useState, useMemo, useCallback, useContext } from 'react';
+import { useRouter } from 'next/router';
+import { useDropzone } from 'react-dropzone';
+import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
-const CreateNFT = () => (
-  <div>CreateNFT</div>
-);
+import { Button } from '../components';
+import images from '../assets';
+
+const CreateNFT = () => {
+  const [fileUrl, setFileUrl] = useState(null);
+  const { theme } = useTheme();
+
+  /// Logic to upload the image on the blockchain (IPFS)
+  const onDrop = useCallback((acceptedFiles) => {}, []);
+
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+    maxSize: 5000000,
+  });
+
+  const fileStyle = useMemo(
+    () => `
+    dark:bg-nft-black-1 bg-white border dark:border-white border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed
+    ${isDragActive ? 'border-file-active' : undefined}
+    ${isDragAccept ? 'border-file-accept' : undefined}
+    ${isDragReject ? 'border-file-reject' : undefined}
+  `,
+    [isDragAccept, isDragActive, isDragReject],
+  );
+
+  return (
+    <div className="flex justify-center sm:px-4 p-12">
+      <div className="w-3/5 md:w-full">
+        <h1 className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-4xl font-semibold ml-4 xs:ml-0">
+          Create a new NFT 📈
+        </h1>
+
+        <div className="mt-16">
+          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
+            Upload File
+          </p>
+
+          <div className="mt-4">
+            <div {...getRootProps()} className={fileStyle}>
+              <input {...getInputProps()} />
+
+              <div className="flexCenter flex-col text-center">
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-xl">
+                  JPG 🏜️ PNG 🏞️ GIF 🌆 SVG 🏙️ WEBM 🌃 --- Max size 100MB.
+                </p>
+
+                <div className="my-12 w-full flex justify-center">
+                  <Image
+                    src={images.upload}
+                    alt="file upload"
+                    width={100}
+                    height={100}
+                    objectFit="contain"
+                    className={theme === 'light' ? 'filter invert' : undefined}
+                  />
+                </div>
+
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">
+                  Drag & Drop your file 📂
+                </p>
+
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm mt-2">
+                  or click to browse files on your device 🗃️
+                </p>
+              </div>
+            </div>
+
+            {/* If the image got successfully uploaded to the blockchain, read it's URL and render it here */}
+            {fileUrl && (
+              <aside>
+                <div>
+                  <img src={fileUrl} alt="asset_fle" />
+                </div>
+              </aside>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default CreateNFT;
