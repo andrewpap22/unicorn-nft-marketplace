cd1b4c588a104844738fcfefee3bfb8fe218558b
Andreas Pappas <andrewpap1997@gmail.com>
2022-09-23 09:21:24 +0300

    deploy our NFTMarketplace smart contract to localhost8545 chain :white_check_mark:

---
+++ context/constants.js
@@ -0,0 +1,6 @@
+import market from './NFTMarketplace.json';
+
+export const MarketAddress = '0x5FbDB2315678afecb367f032d93F642f64180aa3';
+
+/// An Abi is a contract application binary interface. A standard way to interact with a contract in the ethereum ecosystem.
+export const MarketAddressABI = market.abi;
