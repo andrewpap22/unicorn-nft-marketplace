0acdb5c68e4259056e9ee552ea88a2d06d3f874a
Andreas Pappas <andrewpap1997@gmail.com>
2022-10-10 15:42:31 +0300

    create UI for NFT Details page :white_check_mark: have to complete the modal component :construction:

--- pages/my-nfts.jsx
+++ pages/my-nfts.jsx
@@ -32,15 +32,26 @@ const MyNFTs = () => {
 
         <div className="flexCenter flex-col -mt-20 z-0">
           <div className="flexCenter w-40 h-40 sm:w-36 sm:h-36 p-1 bg-nft-black-2 rounded-full">
-            <Image src={images.creator1} className="rounded-full object-cover" objectFit="cover" />
+            <Image
+              src={images.creator1}
+              className="rounded-full object-cover"
+              objectFit="cover"
+            />
           </div>
-          <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl mt-4">{shortenAddress(currentAccount)}</p>
+          <p
+            className="font-poppins dark:text-white text-nft-black-1
+          font-semibold text-2xl mt-4"
+          >
+            {shortenAddress(currentAccount)}
+          </p>
         </div>
       </div>
 
       {!isLoading && !nfts.length ? (
         <div className="flexCenter snm:p-4 p-16">
-          <h1 className="font-poppins dark:text-white text-nft-black-1 font-extrabold text-3xl">No NFTs Owned 😥</h1>
+          <h1 className="font-poppins dark:text-white text-nft-black-1 font-extrabold text-3xl">
+            No NFTs Owned 😥
+          </h1>
         </div>
       ) : (
         <div className="sm:px-4 p-12 w-full minmd:w-4/5 flexCenter flex-col">
@@ -48,11 +59,12 @@ const MyNFTs = () => {
             SearchBar
           </div>
           <div className="mt-3 w-full flex flex-wrap">
-            {nfts.map((nft) => <NFTCard key={nft.token} nft={nft} />)}
+            {nfts.map((nft) => (
+              <NFTCard key={nft.token} nft={nft} />
+            ))}
           </div>
         </div>
       )}
-
     </div>
   );
 };
