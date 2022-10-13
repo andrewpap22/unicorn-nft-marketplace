e8ebb28413b9dd68363c835048a8c2c2063def89
Andreas Pappas <andrewpap1997@gmail.com>
2022-10-13 14:51:09 +0300

    some more bug fixes on navbar component :white_check_mark:

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -11,7 +11,7 @@ import { Button } from '.';
 import images from '../assets';
 
 /// Menu Items component
-const MenuItems = ({ isMobile, active, setActive }) => {
+const MenuItems = ({ isMobile, active, setActive, setIsOpen }) => {
   const generateLink = (index) => {
     switch (index) {
       case 0:
@@ -36,6 +36,8 @@ const MenuItems = ({ isMobile, active, setActive }) => {
           key={index}
           onClick={() => {
             setActive(item);
+
+            if (isMobile) setIsOpen(false);
           }}
           className={`flex flex-row items-center font-poppins font-semibold text-base dark:hover:text-white hover:text-nft-dark mx-3 ${
             active === item
@@ -51,7 +53,7 @@ const MenuItems = ({ isMobile, active, setActive }) => {
 };
 
 /// Button Group component
-const ButtonGroup = ({ setActive, router }) => {
+const ButtonGroup = ({ setActive, router, setIsOpen }) => {
   const { connectWallet, currentAccount } = useContext(NFTContext);
 
   return currentAccount ? (
@@ -60,6 +62,7 @@ const ButtonGroup = ({ setActive, router }) => {
       classStyles="mx-2 rounded-xl"
       handleClick={() => {
         setActive('');
+        setIsOpen(false);
 
         /// redirect to the create NFT page
         router.push('/create-nft');
@@ -107,13 +110,24 @@ const Navbar = () => {
     checkActive(active, setActive, router);
   }, [router.pathname]);
 
+  useEffect(() => {
+    // disable body scroll when navbar is open
+    if (isOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'visible';
+    }
+  }, [isOpen]);
+
   return (
     <nav className="flexBetween w-full fixed z-10 p-4 flex-row border-b dark:bg-nft-dark bg-white dark:border-nft-black-1 border-nft-gray-1">
       <div className="flex flex-1 flex-row justify-start">
         <Link href="/">
           <div
             className="flexCenter md:hidden cursor-pointer"
-            onClick={() => {}}
+            onClick={() => {
+              setActive('Explore NFTs');
+            }}
           >
             <Image
               src={images.logo02}
@@ -130,7 +144,13 @@ const Navbar = () => {
 
         {/* On medium screen devices remove the text and keep only the logo  */}
         <Link href="/">
-          <div className="hidden md:flex" onClick={() => {}}>
+          <div
+            className="hidden md:flex"
+            onClick={() => {
+              setActive('Explore NFTs');
+              setIsOpen(false);
+            }}
+          >
             <Image
               src={images.logo02}
               objectFit="contain"
@@ -172,47 +192,47 @@ const Navbar = () => {
             <ButtonGroup setActive={setActive} router={router} />
           </div>
         </div>
-
       </div>
 
       {/* mobile navigation bar styling... */}
       <div className="hidden md:flex ml-2">
-        {
-          isOpen ? (
-            <Image
-              src={images.cross}
-              objectFit="contain"
-              width={20}
-              height={20}
-              alt="close-menu"
-              onClick={() => setIsOpen(false)}
-              className={theme === 'light' ? 'filter invert' : undefined}
-            />
-          ) : (
-            <Image
-              src={images.menu}
-              objectFit="contain"
-              width={25}
-              height={25}
-              alt="menu"
-              onClick={() => setIsOpen(true)}
-              className={theme === 'light' ? 'filter invert' : undefined}
-            />
-          )
-        }
+        {isOpen ? (
+          <Image
+            src={images.cross}
+            objectFit="contain"
+            width={20}
+            height={20}
+            alt="close-menu"
+            onClick={() => setIsOpen(false)}
+            className={theme === 'light' ? 'filter invert' : undefined}
+          />
+        ) : (
+          <Image
+            src={images.menu}
+            objectFit="contain"
+            width={25}
+            height={25}
+            alt="menu"
+            onClick={() => setIsOpen(true)}
+            className={theme === 'light' ? 'filter invert' : undefined}
+          />
+        )}
 
-        {
-          isOpen && (
-            <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
-              <div className="flex-1 p-4">
-                <MenuItems active={active} setActive={setActive} isMobile />
-              </div>
-              <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
-                <ButtonGroup setActive={setActive} router={router} />
-              </div>
+        {isOpen && (
+          <div className="fixed inset-0 top-65 dark:bg-nft-dark bg-white z-10 nav-h flex justify-between flex-col">
+            <div className="flex-1 p-4">
+              <MenuItems
+                active={active}
+                setActive={setActive}
+                setIsOpen={setIsOpen}
+                isMobile
+              />
             </div>
-          )
-        }
+            <div className="p-4 border-t dark:border-nft-black-1 border-nft-gray-1">
+              <ButtonGroup setActive={setActive} router={router} setIsOpen={setIsOpen} />
+            </div>
+          </div>
+        )}
       </div>
     </nav>
   );
